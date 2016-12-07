e24afd75e304382cbbd40ea2688554a49cdd0ab3
Misha Sawangwan <misha.sawangwan@gmail.com>
2016-12-06 18:05:29 -0800

    updated db code

--- db/db.go
+++ db/db.go
@@ -1,20 +1,56 @@
 package data
 
 import (
-	"github.com/sausheong/gwp/Chapter_2_Go_ChitChat/chitchat/data"
+	"crypto/rand"
+	"crypto/sha1"
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
 	"log"
+)
+
+const (
+	DB_USR  = "postgres"
+	DB_PW   = ""
+	DB_NAME = "unitywebservice"
 )
 
 var PostGreService *sql.DB
 
 func init() {
 	var err error
-	PostGreService, err = sql.Open("postgres", "dbname=unitywebservice sslmode=disable")
+
+	connstr := fmt.Sprintf("user=%s password=%s host=localhost dbname=%s sslmode=disable", DB_USR, DB_PW, DB_NAME)
+	PostGreService, err = sql.Open("postgres", connstr)
+
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	if err = PostGreService.Ping(); err != nil {
 		log.Fatal(err)
 	}
-	return
+
+	log.Printf("[db][db.go][init db: success]\n")
+}
+
+/* based on 'go wp', see: RFC 4122 */
+func CreateUUID() string {
+	u := new([16]byte)
+
+	_, err := rand.Read(u[:])
+
+	if err != nil {
+		log.Fatalln("failed to generate UUID:", err)
+	}
+
+	u[8] = (u[8] | 0x40) & 0x7F
+	u[6] = (u[6] & 0xF) | (0x4 << 4)
+
+	return fmt.Sprintf("%x-%x-%x-%x-%x", u[0:4], u[4:6], u[6:8], u[8:10], u[10:])
+}
+
+/* hash plaintext using SHA-1 */
+func Encrypt(plaintext string) string {
+	return fmt.Sprintf("%x", sha1.Sum([]byte(plaintext)))
 }
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/msawangwan/unitywebservice/model"
+	//"github.com/msawangwan/unitywebservice/data"
 )
 
 var JSON = `{
@@ -11,11 +12,16 @@ var JSON = `{
 }`
 
 func main() {
-	var ss model.SaveState
+	var ss model.PlayerSaveState
 	err := json.Unmarshal([]byte(JSON), &ss)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	fmt.Println(ss)
+	var pp []model.PlayerProfile
+	pp, _ = model.GetPlayersTest()
+	for _, v := range pp {
+		fmt.Println(v.Name)
+	}
 }
