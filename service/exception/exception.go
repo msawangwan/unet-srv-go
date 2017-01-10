41c37442d88d365f0aa4a4177a9b8e3f611e3086
msawangwan <misha.sawangwan@gmail.com>
2017-01-09 17:28:40 -0800

    service/exception: chaned to use pointer recvier

--- service/exception/exception.go
+++ service/exception/exception.go
@@ -2,24 +2,27 @@ package exception
 
 import "fmt"
 
+// Handler is an error type interface
 type Handler interface {
 	Raise(err error, code int)
 	Print() string
 }
 
+// ServerError is a server error abstraction
 type ServerError struct {
 	Error   error
 	Message string
 	Code    int
 }
 
-func (se ServerError) Raise(err error, code int) {
+// Raise implements the exception handler interface
+func (se *ServerError) Raise(err error, code int) {
 	se.Error = err
 	se.Message = err.Error()
 	se.Code = code
 }
 
 // Print implements the exception handler interface
-func (se ServerError) Print() string {
+func (se *ServerError) Print() string {
 	return fmt.Sprintf("[SERVER_ERROR][message: %s][code: %d]", se.Message, se.Code)
 }
