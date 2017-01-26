667c79dcf074efd724438431d9dc178df9c17968
Misha Sawangwan <misha.sawangwan@gmail.com>
2017-01-25 21:40:11 -0800

    data: update tests for accessor method

--- data/data_test.go
+++ data/data_test.go
@@ -22,13 +22,14 @@ func TestGetASpecifictype(t *testing.T) {
 	s, err := MarshallSchema(path)
 	if err != nil {
 		if err != io.EOF {
-			t.Errorf("ERR:: %s", err)
+			t.Errorf("ERR: %s", err)
 		}
 	}
 
-	for k, v := range s.Schemas {
-		if k == "world_position_node" {
-			t.Logf("\n%v", v)
-		}
+	schema, err := s.Access("world_position_node")
+	if err != nil {
+		t.Errorf("ERR: %s", err)
 	}
+
+	t.Logf("%v", schema)
 }
